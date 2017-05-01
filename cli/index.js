afcb632296c559151bd53b5b6c731b53b04e907b
Artur Müller <me@arturmuller.com>
2017-05-01 17:20:33 +0200

    Use launchkit.yml as default config filename

--- cli/index.js
+++ cli/index.js
@@ -16,7 +16,7 @@ cli
   .description(pkg.description)
 
   .command("deploy", "Deploy app")
-  .option("-c, --config <path>", "Override default config path", null, "deploy.yml")
+  .option("-c, --config <path>", "Override default config path", null, "launchkit.yml")
   .option("--key <value>", "Set decryption key inline (falls back to reading key from --key-file)")
   .option("--key-file <path>", "Override default key file path", null, ".key")
   .option("--yes", "Skip verification for protected envs")
@@ -26,19 +26,19 @@ cli
   .command("encrypt", "Encrypts config file")
   .option("--key <value>", "Specify encryption key inline (falls back to reading key from --key-file)")
   .option("--key-file <path>", "Override default key file path", null, ".key")
-  .option("--dest <destination>", "Relative path to where encrypted config file should be written", null, "deploy.yml")
+  .option("--dest <destination>", "Relative path to where encrypted config file should be written", null, "launchkit.yml")
   .argument("<source>", "Relative path from where unecrypted config should be read")
   .action(encrypt)
 
   .command("decrypt", "Decrypts config file")
   .option("--key <value>", "Specify decryption key inline (falls back to reading key from --key-file)")
   .option("--key-file <path>", "Override default key file path", null, ".key")
-  .option("--dest <destination>", "Relative path to where decrypted config file should be written", null, "deploy.decrypted.yml")
+  .option("--dest <destination>", "Relative path to where decrypted config file should be written", null, "launchkit.decrypted.yml")
   .argument("<source>", "Relative path from where ecrypted config should be read")
   .action(decrypt)
 
   .command("view", "Prints decrypted config (or specific env) to stdout")
-  .option("-c, --config <path>", "Override default config path", null, "deploy.yml")
+  .option("-c, --config <path>", "Override default config path", null, "launchkit.yml")
   .option("--key <value>", "Specify decryption key inline (falls back to reading key from --key-file)")
   .option("--key-file <path>", "Override default key file path", null, ".key")
   .option("-d, --with-default", "Merge env with default")
@@ -46,13 +46,13 @@ cli
   .action(view)
 
   .command("protect", "Indicates specfied env as protected")
-  .option("-c, --config <path>", "Override default config path", null, "deploy.yml")
+  .option("-c, --config <path>", "Override default config path", null, "launchkit.yml")
   .argument("<env>", "Name of env for which to update keys")
   .action(protect)
 
   .command("update", "Updates and encrypts specified keys on config")
   .alias("add")
-  .option("-c, --config <path>", "Override default config path", null, "deploy.yml")
+  .option("-c, --config <path>", "Override default config path", null, "launchkit.yml")
   .option("--key <value>", "Specify decryption key inline (falls back to reading key from --key-file)")
   .option("--key-file <path>", "Override default key file path", null, ".key")
   .argument("<env>", "Name of env for which to update keys")
@@ -60,7 +60,7 @@ cli
   .action(update)
 
   .command("remove", "Removes specified keys from config")
-  .option("-c, --config <path>", "Override default config path", null, "deploy.yml")
+  .option("-c, --config <path>", "Override default config path", null, "launchkit.yml")
   .argument("<env>", "Name of env from which to remove keys")
   .argument("<keys...>", "Keys to remove form env. Supply as `path.to.key`")
   .action(remove);
